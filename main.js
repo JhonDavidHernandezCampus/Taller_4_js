5ef5c4bbfa106f601b1c3a4d5938c692e49b292f
JhonDavidHernandezCampus <yonif@gmail.com>
2023-03-20 14:57:03 -0500

    feat: :fire: Comienzo del taller 4
    se maqueto el menu en un alert

---
+++ main.js
@@ -0,0 +1,21 @@
+
+let vlrMenu = prompt(alert(`-------MENU------
+    1. Lectura de datos
+    2. Crear un objeto
+    3. Mostrar Objeto
+    4. Crear Array
+    5. Mostrar Array
+    6. Elimine primer elemento del Aray
+    7. Elimine el ultimo elemento del Array
+    8. Eliminar cualquier elemento del Aray
+    9. Agregar un elemento al comienzo del Array
+    10. Agregar elemento al final del Array
+    11. Crear Array con Objetos
+    12. Iterar Array con Objetos con FOR
+    13. Iterar Array con Objetos con ForEach
+    14. Iterar Array con Objetos con Map y crear copia
+    15. Proceso personal
+    0. Salir`));
+
+alert(`Muestro el valor del menu ${vlrMenu}`);
+console.log("es este?",vlrMenu);
